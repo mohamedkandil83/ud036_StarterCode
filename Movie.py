65b6cac0ad735c764592088d6a9e639f95b0df0b
Mohamed Kandil <mohamed.kandil-attia@vodafone.com>
2018-01-03 00:46:57 +0200

    Apply Code Review Comments

--- Movie.py
+++ Movie.py
@@ -1,19 +1,60 @@
 import fresh_tomatoes
 
+
 class Movie():
+	""" The class will be used to create instances of Movie object """
 
-        def __init__ (self, title, poster_image_url, trailer_youtube_url):
-                self.title = title
-                self.poster_image_url = poster_image_url
-                self.trailer_youtube_url = trailer_youtube_url
+        def __init__(self, title, poster_image_url, trailer_youtube_url):
+                """ Predefined function to set the instance variables of Movie Class """
+            self.title = title
+            self.poster_image_url = poster_image_url
+            self.trailer_youtube_url = trailer_youtube_url
 
-terminal = Movie("Terminal","http://movizland.com/wp-content/uploads/terminal_ver2.jpg","https://www.youtube.com/watch?v=ciByvddyHBs")
-toy_story = Movie("Toy Story","https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg","https://www.youtube.com/watch?v=4KPTXpQehio")
-nemo = Movie("The Finding Nemo","https://www.kids.almo7eb.com/thumbs/nemo-8344537722.jpg","https://www.youtube.com/watch?v=wZdpNglLbt8")
-iron_giant = Movie("The Iron Giant","http://t1.gstatic.com/images?q=tbn:ANd9GcRCwz75tuERvCFpNaHIeuqr6NSh_KjdfCxcq3i2M3u47FQBI0AS","https://www.youtube.com/watch?v=JgjmFBX34zc")
-bambi = Movie("Bambi ","https://www.kids.almo7eb.com/thumbs/bambi-1-1124813351.jpg","https://www.youtube.com/watch?v=suAZVUatuH0")
-incredibles = Movie("The Incredibles","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXYncfHfNmFpDBk7CYQ9KDQCu9FdZjyXsvPr2Ip8VqVghScLZm7Q","https://www.youtube.com/watch?v=ZJDMWVZta3M")
+# Terminal movie: movie title, movie poster image url, movie youtube trailer url
+terminal = Movie(
+"Terminal", 
+"http://movizland.com/wp-content/uploads/terminal_ver2.jpg", 
+"https://www.youtube.com/watch?v=ciByvddyHBs")
 
-movies = [terminal, toy_story, nemo, iron_giant, bambi, incredibles]
+# Toy story movie: movie title, movie poster image url, movie youtube trailer url
+toy_story = Movie(
+"Toy Story", 
+"https://goo.gl/bj4aZS", 
+"https://www.youtube.com/watch?v=4KPTXpQehio")
 
+# The Finding Nemo movie: movie title, movie poster image url, movie youtube trailer url
+nemo = Movie(
+"The Finding Nemo", 
+"https://www.kids.almo7eb.com/thumbs/nemo-8344537722.jpg", 
+"https://www.youtube.com/watch?v=wZdpNglLbt8")
+
+# The Iron Giant movie: movie title, movie poster image url, movie youtube trailer url
+iron_giant = Movie(
+"The Iron Giant", 
+"https://goo.gl/CP5Pfz", 
+"https://www.youtube.com/watch?v=JgjmFBX34zc")
+
+# Bambi movie: movie title, movie poster image url, movie youtube trailer url
+bambi = Movie(
+"Bambi", 
+"https://goo.gl/YBdhzS", 
+"https://www.youtube.com/watch?v=suAZVUatuH0")
+
+# The Incredibles movie: movie title, movie poster image url, movie youtube trailer url
+incredibles = Movie(
+"The Incredibles", 
+"https://goo.gl/rU9u8d", 
+"https://www.youtube.com/watch?v=ZJDMWVZta3M")
+
+# Array of movies that will be passed to open movies function
+movies = [
+	terminal, 
+	toy_story, 
+	nemo, 
+	iron_giant, 
+	bambi, 
+	incredibles
+	]
+
+# Open movies page in the a web browser to display movies list
 fresh_tomatoes.open_movies_page(movies)
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ud036_StarterCode
 Source code for a Movie Trailer website.
+
+# How to run this code
+To run this project you have to install python first
+
+1- Download the code or clone my repository
+2- Open Movie.py file on IDLE
+3- Run Module (F5)
+4- Enjoy the trailers for my favourite movies list
